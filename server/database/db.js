a2720209e74079b4ab1fc3ac1b6296b6702dedb1
Paras Sachdeva <parassachdeva0111@gmail.com>
2024-03-10 22:57:37 +0530

    minor bug fix

--- server/database/db.js
+++ server/database/db.js
@@ -8,7 +8,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
 
-    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-hphkfqv-shard-00-00.bmuzxat.mongodb.net:27017,ac-hphkfqv-shard-00-01.bmuzxat.mongodb.net:27017,ac-hphkfqv-shard-00-02.bmuzxat.mongodb.net:27017/?ssl=true&replicaSet=atlas-ifygh4-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.cptffbb.mongodb.net/?retryWrites=true&w=majority`;
 
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
@@ -20,7 +20,7 @@ const Connection = () => {
         console.log('Database disconnected');
     })
 
-    mongoose.connection.on('error', () => {
+    mongoose.connection.on('error', (error) => {
         console.log('Error while connecting with the database ', error.message);
     })
 }
